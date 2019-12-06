84de552d4381d7a8c7864f3cad5b89de53389c23
Leslie Cheng <leslie.cheng5@gmail.com>
2019-12-06 05:42:55 -0800

    Spaces instead of tabs, oops vim.

--- day6/part1/sol.java
+++ day6/part1/sol.java
@@ -1,45 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-class a {
+class sol {
 
-	static Map<String, List<String>> map;
-	public static void main(String[] args) throws Exception {
-		File file = new File("input.txt");
+    static Map<String, List<String>> map;
 
-		BufferedReader br = new BufferedReader(new FileReader(file));
+    public static void main(String[] args) throws Exception {
+        File file = new File("input.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
         // Read lines
         List<String> data = new ArrayList<>();
-		String line = br.readLine();
-		while (line != null) {
-			data.add(line);
-			line = br.readLine();
+        String line = br.readLine();
+        while (line != null) {
+            data.add(line);
+            line = br.readLine();
         }
-		br.close();
+        br.close();
 
-		// Construct a map of planet to the other
-		map = new HashMap<>();
-		
-		for (String s : data) {
-			String[] splits = s.split("\\)");
-			String first = splits[0];
-			String second = splits[1];
+        // Construct a map of planet to the other
+        map = new HashMap<>();
 
-			map.computeIfAbsent(first, key -> new ArrayList<>()).add(second);
-		}
+        for (String s : data) {
+            String[] splits = s.split("\\)");
+            String first = splits[0];
+            String second = splits[1];
 
-		System.out.println(count("COM", 0));
-	}
+            map.computeIfAbsent(first, key -> new ArrayList<>()).add(second);
+        }
 
-	public static long count(String planet, long depth) {
-		if (!map.containsKey(planet)) {
-			return depth;
-		}
-		long c = 0;
-		for (String p : map.get(planet)) {
-			c += count(p, depth + 1);
-		}
-		return depth + c;
-	}
+        System.out.println(count("COM", 0));
+    }
+
+    public static long count(String planet, long depth) {
+        if (!map.containsKey(planet)) {
+            return depth;
+        }
+        long c = 0;
+        for (String p : map.get(planet)) {
+            c += count(p, depth + 1);
+        }
+        return depth + c;
+    }
 }
--- starter.cpp
+++ starter.cpp
@@ -6,29 +6,29 @@
 using namespace std;
 
 int main() {
-	ifstream file;
-	file.open("input.txt");
+    ifstream file;
+    file.open("input.txt");
 
-	// Read lines
-	// vector<string> data;
-	// if (file.is_open()) {
-	// 	string line;
-	// 	while (!file.eof()) {
-	// 		getline(file, line);
-	// 		data.push_back(line);
-	// 	}
-	// }
-	// file.close();
+    // Read lines
+    // vector<string> data;
+    // if (file.is_open()) {
+    // 	string line;
+    // 	while (!file.eof()) {
+    // 		getline(file, line);
+    // 		data.push_back(line);
+    // 	}
+    // }
+    // file.close();
 
-	// Read entire file
-	// string data;
-	// if (file.is_open()) {
-	// 	char a;
-	// 	while (file.get(a)) {
-	// 		data += a;
-	// 	}
-	// }
-	// file.close();
+    // Read entire file
+    // string data;
+    // if (file.is_open()) {
+    // 	char a;
+    // 	while (file.get(a)) {
+    // 		data += a;
+    // 	}
+    // }
+    // file.close();
 
     return 0;
 }
